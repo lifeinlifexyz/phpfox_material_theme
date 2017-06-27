665d0a597db41168597936d470446f82bb58fea5
Mirzhan <mick912@mail.ru>
2017-06-27 06:26:48 +0000

    start.php deleted online with Bitbucket

--- start.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//if (!Phpfox::isUser()) {
-//    /**
-//     * @var $oTpl Phpfox_Template
-//     */
-//    $oTpl = Phpfox_Template::instance();
-//    $oTpl->setLayout('guest_layout');
-//}
